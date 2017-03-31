614a169e2fa3931210c4548f3b5c071cb8960db1
gotaejong <taejonggo@tsis.co.kr>
2017-04-01 01:49:02 +0900

    by proidea

---
+++ forever.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ $1 = "start" ]
+then
+	forever start target/assets/js/server.js
+elif [ $1 = "stop" ]
+then
+	forever stop target/assets/js/server.js
+elif [ $1 = "stop" ]
+then
+	forever restart target/assets/js/server.js
+elif [ $1 = "status" ]
+then
+	forever restart target/assets/js/server.js
+else
+	echo "sh forever.sh start|stop|restart"
+fi
