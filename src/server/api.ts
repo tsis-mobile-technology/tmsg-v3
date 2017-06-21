e4a80cffbb885f9a77595454bf33bbf29ef3e558
gotaejong <taejonggo@tsis.co.kr>
2017-06-21 22:03:31 +0900

    bug fix

--- src/server/api.ts
+++ src/server/api.ts
@@ -354,7 +354,8 @@ class ApiServer {
                     });
                 } else if( updateType == "NAME" ) {
                         const spawn = require('child_process').spawn;
-                        const ls = spawn('/home/proidea/workspaceHTML5/tmsg-v3/shorturl');
+                        //const ls = spawn('/home/proidea/workspaceHTML5/tmsg-v3/shorturl');
+                        const ls = spawn('/Users/gotaejong/projects/WorkspacesHTML5/tmsg-v3/shorturl');
 
                         ls.stdout.on('data', (data) => {
                             console.log(`stdout: ${data}`);
@@ -363,11 +364,19 @@ class ApiServer {
                                 // 1. send SMS customer phone
                                 // 2. DB Update
                                 // const client = socketIoClient.connect(mtURL, options);
-                                var sendData = this.zeroLeftPad(mtMessage.length, 5) + mtMessage;
+
+                                var messageSize = mtMessage.length+"";
+                                while (messageSize.length < 5) messageSize = "0" + messageSize;
+
+                                var sendData = messageSize + mtMessage;
                                 var socketClient = socketIoClient(mtURL);
-                                socketClient.on('connect', function() {});
-                                socketClient.on('event', function(sendData) {});
-                                socketClient.on('disconnect', function() {});
+                                socketClient.connect();
+                                socketClient.send(sendData);
+                                socketClient.disconnect();
+
+                                // socketClient.on('connect', function() {});
+                                // socketClient.on('event', function(sendData) {});
+                                // socketClient.on('disconnect', function() {});
 
                                 pool.query('UPDATE TB_AUTOCHAT_CUSTOMER SET NAME = ?, YN_AUTH = ?, ETC1 = ? WHERE UNIQUE_ID = ?', [content, "N", nOTP, user_key], function(err, rows, fields) {
                                     if(err) console.log("Query Error:", err);
@@ -483,12 +492,6 @@ class ApiServer {
 
         let kakaoSocket = new KakaoSocket(this.kakao_io);
    }
-
-    private zeroLeftPad(num:number, size:number): string {
-        var s = num+"";
-        while (s.length < size) s = "0" + s;
-        return s;
-    }
 
     // Start HTTP server listening
     private kakaoListen(): void {
